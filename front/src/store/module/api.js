6e1affb0fd576d0ba2597369f0bd835788bed818
xingzhe <54618163+2892211452@users.noreply.github.com>
2022-08-27 19:43:02 +0800

    加入可视化前端

---
+++ front/src/store/module/api.js
@@ -0,0 +1,14 @@
+const api = {
+  state: {
+    baseURL: "http://127.0.0.1:80/",
+    port: "5000",
+    birthdayURL: "",
+    taskAddURL: "task_add",
+    taskDeleteURL: "task_delete",
+  },
+  mutations: {},
+  actions: {},
+  getters: {},
+};
+
+export default api;
